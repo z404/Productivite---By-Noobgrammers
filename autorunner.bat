73748fbe619aee852f51d9660b212328834404f0
z404 <anishr890@gmail.com>
2021-04-09 23:25:44 +0530

    Initial django barebones commit

---
+++ autorunner.bat
@@ -0,0 +1,3 @@
+python.exe manage.py makemigrations
+python.exe manage.py migrate
+python.exe manage.py runserver
---
+++ mailit/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MailitConfig(AppConfig):
+    name = 'mailit'
