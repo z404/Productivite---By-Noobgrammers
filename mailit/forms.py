e114a810c3be1cbee3368219ea50381ac813d38b
sankalpmukim <sankalpmukim@gmail.com>
2021-04-10 18:26:25 +0530

    compose.html

--- mailit/forms.py
+++ mailit/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from .backend import getuseremailid
+# from .backend import getuseremailid
 
 
 class ComposeForm(forms.Form):
     sender = forms.EmailField(
-        initial=getuseremailid(), auto_id=True, label='Sender:'help_text='Your email address, please.', disabled=True)
+        initial='example@example.com', auto_id=True, label='Sender:'help_text='Your email address, please.', disabled=True)
     reciever = forms.EmailField(
         auto_id=True, label='Reciever', help_text='A valid email address, please.')
     cc = forms.EmailField(auto_id=True, required=False, label='CC')
